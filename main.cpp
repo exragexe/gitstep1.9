283729b9ed1974a62b92316f1f0270dea07d169e
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 22:51:58 +0200

    ex2done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+
+using namespace std;
+int rows(int num,char znak = '*') {
+    if (num == 0) {
+        cout << endl;
+        return 0;
+    }
+    cout << znak << " ";
+    rows(num-1, znak);
+}
+int main() {
+    int n;
+    cout <<"Enter number of stars: ";
+    cin >> n;
+    cout << rows(n);
+    return 0;
+}
