a30a44f13f2644823da5ce7abeca813fab1612d4
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 22:32:57 +0200

    ex1done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 
+
+#include <iostream>
+using namespace std;
+int st(int num, int stepen1, int i=0){
+    int result = 1;
+    for (; i < stepen1; i++) {
+        result *= num;
+    }
+    return result;
+}
+int main(){
+    int n,k;
+    cout <<"Enter number: ";
+    cin>>n;
+    cout <<"Enter stepen: ";
+    cin >>k;
+
+    cout <<"Result: " << st(n,k);
+
+    return 0;
+}
