5caec5269d050056fc5791b6186f42d495eee343
- <78488184+exragexe@users.noreply.github.com>
2023-02-11 14:43:46 +0200

    ex3done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 
+
+#include <iostream>
+using namespace std;
+int suma(int start, int end,int sum=0){
+
+    if (start > end) {
+        return sum;
+    }
+    else if(start <= end) {
+        sum += start;
+        start++;
+        return suma(start, end, sum);
+    }
+}
+int main(){
+    int a,b;
+    cout <<"Enter a: ";
+    cin>>a;
+    cout <<"Enter b: ";
+    cin >>b;
+    cout <<"Result: " << suma(a,b);
+
+    return 0;
+}
